ad4e3bdd3fb224fc47c106318c4d7ecdbb0df9af
superartstudio <superartstudio@f8d1fa10-fe46-0410-826c-79b63ec2d5d2>
2008-02-26 07:24:38 +0000

    added toolkit idx2lua

--- leo_dict.lua
+++ leo_dict.lua
@@ -1,3 +1,9 @@
+dict_name = '/usr/share/stardict/dic/stardict-langdao-ec-gb-2.4.2/langdao-ec-gb.idx';
+
+dict_file = io.open(dict_name, 'rb');
+dict_data = dict_file:read("*all");
+dict_file:close();
+
 function leo_dict_init()
 	-- add our very first chat command!
 	SlashCmdList["LEODICT"] = leo_dict_cmd;
@@ -9,3 +15,11 @@ function leo_dict_cmd(msg)
 	-- this function handles our chat command
 	message(msg);
 end 
+
+--	dev warp
+
+function message(m)
+	print(m);
+end
+
+leo_dict_cmd(dict_data);
