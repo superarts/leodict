d46e0ff7ec14e9c4ce1387d745aecd371d80ef10
superartstudio <superartstudio@f8d1fa10-fe46-0410-826c-79b63ec2d5d2>
2008-02-26 10:30:18 +0000

    updated the main plugin file

--- trunk/leo_dict.lua
+++ trunk/leo_dict.lua
@@ -1,23 +1,26 @@
-dict_name = '/usr/share/stardict/dic/stardict-langdao-ec-gb-2.4.2/langdao-ec-gb.idx';
-
-dict_file = io.open(dict_name, 'rb');
-dict_data = dict_file:read("*all");
-dict_file:close();
+--	dict_name = '/usr/share/stardict/dic/stardict-langdao-ec-gb-2.4.2/langdao-ec-gb.idx';
 
 function leo_dict_init()
-	-- add our very first chat command!
+	--	init dictionary
+	f = io.open('dict/longdao_enus2zhcn.txt');
+	s = f:read('*all');
+	f:close();
+	--	add our very first chat command!
 	SlashCmdList["LEODICT"] = leo_dict_cmd;
 	SLASH_LEODICT1 = "/leodict";
 	SLASH_LEODICT2 = "/ld";
 end
 
 function leo_dict_cmd(msg)
-	-- this function handles our chat command
-	message(msg);
+	--	this function handles our chat command
+	_, _, msg, meaning = string.find(s, '\t(' .. sub .. ')\t\n(.-)\t\n');
+	--	print(index);
+	--	print(meaning);
+	message(meaning);
 end 
 
 --	dev warp
-
+--[[
 function message(m)
 	print(m);
 end
@@ -26,4 +29,4 @@ test = {};
 test['test'] = 'my file';
 
 leo_dict_cmd(test['test']);
-
+]]
