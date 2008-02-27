9a65c166c6f0802f96b88431844c1b893d107cd2
superartstudio <superartstudio@f8d1fa10-fe46-0410-826c-79b63ec2d5d2>
2008-02-27 06:43:05 +0000

    added lua-format dictionary

--- leo_dict.lua
+++ leo_dict.lua
@@ -1,32 +1,31 @@
 --	dict_name = '/usr/share/stardict/dic/stardict-langdao-ec-gb-2.4.2/langdao-ec-gb.idx';
 
 function leo_dict_init()
-	--	init dictionary
-	f = io.open('dict/longdao_enus2zhcn.txt');
-	s = f:read('*all');
-	f:close();
-	--	add our very first chat command!
+	--	init dictionary: io library not allowed in wow, disabled
+	if false then
+		f = io.open('dict/longdao_enus2zhcn.txt');
+		dict_data = f:read('*all');
+		f:close();
+	end;
+
 	SlashCmdList["LEODICT"] = leo_dict_cmd;
 	SLASH_LEODICT1 = "/leodict";
 	SLASH_LEODICT2 = "/ld";
 end
 
 function leo_dict_cmd(msg)
-	--	this function handles our chat command
-	_, _, msg, meaning = string.find(s, '\t(' .. sub .. ')\t\n(.-)\t\n');
-	--	print(index);
-	--	print(meaning);
-	message(meaning);
+	_, _, msg, meaning = string.find(dict_data, '\t(' .. msg .. ')\t\n(.-)\t\n');
+	message(msg .. ': ' .. meaning);
 end 
 
---	dev warp
---[[
-function message(m)
-	print(m);
+if true then
+	function message(m)
+		print(m);
+	end
+
+	--	testing code
+	require("dict/longdao_enus2zhcn");
+	SlashCmdList = {};
+	leo_dict_init();
+	leo_dict_cmd('test');
 end
-
-test = {};
-test['test'] = 'my file';
-
-leo_dict_cmd(test['test']);
-]]
