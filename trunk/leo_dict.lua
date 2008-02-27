a5ebe594933219feaddd9198287e35ed45f38e44
superartstudio <superartstudio@f8d1fa10-fe46-0410-826c-79b63ec2d5d2>
2008-02-27 16:19:52 +0000

    added -hide and -show function

--- trunk/leo_dict.lua
+++ trunk/leo_dict.lua
@@ -13,8 +13,22 @@ function leo_dict_init()
 end
 
 function leo_dict_cmd(msg)
-	_, _, msg, meaning = string.find(dict_data, '\t(' .. msg .. ')\t\n(.-)\t\n');
-	message(msg .. ': ' .. meaning);
+	if msg == '-show' then
+		leo_dict_core:Show();
+	elseif msg == '-hide' then
+		leo_dict_core:Hide();
+	else
+		_, _, _, meaning = string.find(dict_data, '\t(' .. msg .. ')\t\n(.-)\t\n');
+
+		if meaning ~= nil then
+			s = msg .. ': ' .. meaning;
+		else
+			s = 'Not found: ' .. msg;
+		end
+
+		leo_dict_data:SetText(s);
+		--	message(msg .. ': ' .. meaning);
+	end
 end 
 
 --	for local testing
